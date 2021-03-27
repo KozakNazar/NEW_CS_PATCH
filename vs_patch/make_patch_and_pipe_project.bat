fb4a8be3f6ab5346a796345bc7dd79915229972a
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2021-03-27 21:18:00 +0200

    Add files via upload

---
+++ vs_patch/make_patch_and_pipe_project.bat
@@ -0,0 +1,15 @@
+rem ###########################
+rem  N.Kozak // Lviv'2021
+rem ############################
+
+xcopy  .\src\numeric_bit\fx\scfx_mant.h          ..\src\numeric_bit\fx\  /y /q
+xcopy  .\src\fstream.h                           ..\src\                 /y /q
+xcopy  .\src\iostream.h                          ..\src\                 /y /q
+xcopy  .\src\sc_bv.h                             ..\src\                 /y /q
+xcopy  .\src\sc_lv.h                             ..\src\                 /y /q
+xcopy  .\src\sc_signal_array.h                   ..\src\                 /y /q
+xcopy  .\src\sc_wait.cpp                         ..\src\                 /y /q
+xcopy  .\cmake_projects\pipe\CMakeSettings.json  ..\                     /y /q
+xcopy  .\cmake_projects\pipe\CMakeLists.txt      ..\                     /y /q
+
+pause
